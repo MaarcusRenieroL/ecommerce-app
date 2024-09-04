6e1c61b89fcf2547d45078405c0315d68030c2ba
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-09-04 21:39:36 +0530

    init: init shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-	return (
-		<div>
-			<h1 className="text-red-500 font-bold text-3xl underline">E Commerce Application</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1 className="text-red-500 font-bold text-3xl underline">
+        E Commerce Application
+      </h1>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
+
