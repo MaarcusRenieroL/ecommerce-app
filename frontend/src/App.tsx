811cae44fc969973ae1fd0d05538b88762c7ca9a
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-09-05 14:45:50 +0530

    feat: add ui for individual product page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from "@/pages/home-page.tsx";
 import { ProductsPage } from "@/pages/products-page.tsx";
 import { CategoriesPage } from "@/pages/categories-page.tsx";
-import {DealsPage} from "@/pages/deals-page.tsx";
+import { DealsPage } from "@/pages/deals-page.tsx";
+import { ProductPage } from "@/pages/product-page.tsx";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
 				<Route path="/products" element={<ProductsPage />} />
 				<Route path="/categories" element={<CategoriesPage />} />
 				<Route path="/deals" element={<DealsPage />} />
+				<Route path="/product" element={<ProductPage />} />
 			</Routes>
 		</Router>
   );
--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,13 +1,12 @@
 export type Product = {
-	id: number,
-	name: string,
-	image: string,
-	rating: number,
-	price: number
+	id: number;
+	name: string;
+	image: string;
+	rating: number;
+	price: number;
 }
 
 export type Filter = {
-	id: number,
-	name: string,
-	
+	id: number;
+	name: string;
 }
