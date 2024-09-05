0a0b91e938a4a056934bae568960ee63e2c734c8
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-09-05 12:53:02 +0530

    feat: add ui for categories page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 import HomePage from "@/pages/home-page.tsx";
-import {ProductsPage} from "@/pages/products-page.tsx";
+import { ProductsPage } from "@/pages/products-page.tsx";
+import { CategoriesPage } from "@/pages/categories-page.tsx";
 
 export default function App() {
   return (
@@ -8,6 +10,7 @@ export default function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/products" element={<ProductsPage />} />
+				<Route path="/categories" element={<CategoriesPage />} />
 			</Routes>
 		</Router>
   );
