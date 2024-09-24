3efa19aacf51d9d265d7d47d2ac8198807f2da31
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-09-24 14:43:42 +0530

    fix: fix add new address modal according to the schema in the backend

--- frontend/src/components/pages/personalized-content/your-addresses-page/add-new-address-modal.tsx
+++ frontend/src/components/pages/personalized-content/your-addresses-page/add-new-address-modal.tsx
@@ -1,128 +1,218 @@
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+	Dialog, DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "@/components/ui/dialog";
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { addressSchema } from "@/lib/zod-schema.ts";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form.tsx";
+import { Textarea } from "@/components/ui/textarea.tsx";
 
 export const AddNewAddressModal = () => {
-  const [open, setOpen] = useState(false);
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button>Add New Address</Button>
-        </DialogTrigger>
-        <DialogContent className="max-w-7xl">
-          <DialogHeader>
-            <DialogTitle>Add new address</DialogTitle>
-            <DialogDescription>
-              Fill out the form to add a new address
-            </DialogDescription>
-          </DialogHeader>
-          <ProfileForm />
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline">Add New Address</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Add new address</DrawerTitle>
-          <DrawerDescription>
-            Fill out the form to add a new address
-          </DrawerDescription>
-        </DrawerHeader>
-        <ScrollArea className="h-96 p-10">
-          <ProfileForm />
-        </ScrollArea>
-        <DrawerFooter className="pt-2">
-          <Button>Save</Button>
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  );
+	const [ open, setOpen ] = useState(false);
+	const isDesktop = useMediaQuery("(min-width: 768px)");
+	
+	if (isDesktop) {
+		return (
+			<Dialog open={open} onOpenChange={setOpen}>
+				<DialogTrigger asChild>
+					<Button>Add New Address</Button>
+				</DialogTrigger>
+				<DialogContent className="max-w-5xl">
+					<DialogHeader>
+						<DialogTitle>Add new address</DialogTitle>
+						<DialogDescription>
+							Fill out the form to add a new address
+						</DialogDescription>
+					</DialogHeader>
+					<ProfileForm/>
+				</DialogContent>
+			</Dialog>
+		);
+	}
+	return (
+		<Drawer open={open} onOpenChange={setOpen}>
+			<DrawerTrigger asChild>
+				<Button>Add New Address</Button>
+			</DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader className="text-left">
+					<DrawerTitle>Add new address</DrawerTitle>
+					<DrawerDescription>
+						Fill out the form to add a new address
+					</DrawerDescription>
+				</DrawerHeader>
+				<ScrollArea className="h-96 p-4">
+					<ProfileForm/>
+				</ScrollArea>
+			</DrawerContent>
+		</Drawer>
+	);
 };
 
 function ProfileForm() {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-4">
-      <div className="w-full space-y-2">
-        <Label className="text-right">Full Name</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Phone Number</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Alternate Phone Number</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Address Line 1</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Address Line 2</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Address Line 3</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Landmark</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Town / City</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">State</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2">
-        <Label className="text-right">Pincode</Label>
-        <Input />
-      </div>
-      <div className="w-full space-y-2 md:col-span-2 lg:col-span-3">
-        <Label className="text-right">Instructions</Label>
-        <Textarea />
-      </div>
-    </div>
-  );
+	const form = useForm<z.infer<typeof addressSchema>>({
+		resolver: zodResolver(addressSchema),
+		defaultValues: {
+			number: "",
+			street: "",
+			city: "",
+			state: "",
+			postalCode: "",
+			country: "",
+			instructions: "",
+			landmark: "",
+			default: false,
+		}
+	});
+	
+	const onSubmit = (data: z.infer<typeof addressSchema>) => {
+		console.log(data);
+	}
+	
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-4 p-4">
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>House / Building / Apartment Number</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="number"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Street</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="street"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>City</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="city"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>State</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="state"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Country</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="country"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Postal Code</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="postalCode"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Landmark</FormLabel>
+								<FormControl>
+									<Input {...field} placeholder="" type="text" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="landmark"
+						control={form.control}
+					/>
+					
+					<FormField
+						render={({ field }) => (
+							<FormItem className="md:col-span-2 lg:col-span-3">
+								<FormLabel>Instructions</FormLabel>
+								<FormControl>
+									<Textarea {...field} placeholder="" />
+								</FormControl>
+								<FormMessage/>
+							</FormItem>
+						)}
+						name="instructions"
+						control={form.control}
+					/>
+					
+				</div>
+				
+				<div className="flex items-end justify-end w-full gap-5">
+					<DialogClose>
+						<Button variant="outline">Cancel</Button>
+					</DialogClose>
+					<Button>Save</Button>
+				</div>
+			</form>
+		</Form>
+	);
 }
--- frontend/src/lib/zod-schema.ts
+++ frontend/src/lib/zod-schema.ts
@@ -3,92 +3,132 @@
 import { z } from "zod";
 
 export const signUpSchema = z
-  .object({
-    firstName: z
-      .string({
-        required_error: "First name is required",
-      })
-      .min(3, {
-        message: "First name must be at least 3 characters long",
-      }),
-    lastName: z
-      .string({
-        required_error: "Last name is required",
-      })
-      .min(3, {
-        message: "Last name must be at least 3 characters long",
-      }),
-    username: z
-      .string({
-        required_error: "Email is required",
-      })
-      .email({
-        message: "Invalid email",
-      }),
-    password: z
-      .string({
-        required_error: "Password is required",
-      })
-      .min(6, {
-        message: "Password must be at least 6 characters long",
-      }),
-    confirmPassword: z
-      .string({
-        required_error: "Confirm Password is required",
-      })
-      .min(6, {
-        message: "Confirm Password must be at least 6 characters long",
-      }),
-    phoneNumber: z
-      .string({
-        required_error: "Phone number is required",
-      })
-      .min(10, {
-        message: "Phone number must be at least 10 characters long",
-      })
-      .max(15, {
-        message: "Phone number must not be more than 15 characters long",
-      }),
-    role: z.string(),
-    hasBusinessAccount: z.boolean(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ["password"],
-  });
+	.object({
+		firstName: z
+			.string({
+				required_error: "First name is required",
+			})
+			.min(3, {
+				message: "First name must be at least 3 characters long",
+			}),
+		lastName: z
+			.string({
+				required_error: "Last name is required",
+			})
+			.min(3, {
+				message: "Last name must be at least 3 characters long",
+			}),
+		username: z
+			.string({
+				required_error: "Email is required",
+			})
+			.email({
+				message: "Invalid email",
+			}),
+		password: z
+			.string({
+				required_error: "Password is required",
+			})
+			.min(6, {
+				message: "Password must be at least 6 characters long",
+			}),
+		confirmPassword: z
+			.string({
+				required_error: "Confirm Password is required",
+			})
+			.min(6, {
+				message: "Confirm Password must be at least 6 characters long",
+			}),
+		phoneNumber: z
+			.string({
+				required_error: "Phone number is required",
+			})
+			.min(10, {
+				message: "Phone number must be at least 10 characters long",
+			})
+			.max(15, {
+				message: "Phone number must not be more than 15 characters long",
+			}),
+		role: z.string(),
+		hasBusinessAccount: z.boolean(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords don't match",
+		path: [ "password" ],
+	});
+
+export const addressSchema = z.object({
+	number: z.string({
+		required_error: "House / Building / Apartment number is required",
+	}),
+	street: z.string({
+		required_error: "Street is required",
+	}).min(1, {
+		message: "Street is required"
+	}),
+	city: z.string({
+		required_error: "Street is required"
+	}).min(1, {
+		message: "City is required"
+	}),
+	state: z.string({
+		required_error: "Street is required"
+	}).min(2, {
+		message: "State is required"
+	}),
+	postalCode: z.string({
+		required_error: "Street is required"
+	}).min(5, {
+		message: "Zip code is required"
+	}),
+	country: z.string({
+		required_error: "Street is required"
+	}).min(2, {
+		message: "Country is required"
+	}),
+	instructions: z.string().min(2, { message:
+			"Instructions is required"
+	}).optional(),
+	landmark: z.string().min(2, {
+		message: "Landmark is required"
+	}).optional(),
+	default: z.boolean({
+		required_error: "Default address is required",
+	})
+})
 
 export const signInSchema = z.object({
-  email: z
-    .string({
-      required_error: "Email is required",
-    })
-    .email({
-      message: "Invalid email",
-    }),
-  password: z
-    .string({
-      required_error: "Password is required",
-    })
-    .min(6, {
-      message: "Password must be at least 6 characters long",
-    }),
+	email: z
+		.string({
+			required_error: "Email is required",
+		})
+		.email({
+			message: "Invalid email",
+		}),
+	password: z
+		.string({
+			required_error: "Password is required",
+		})
+		.min(6, {
+			message: "Password must be at least 6 characters long",
+		}),
 });
 
 export const businessSchema = z.object({
-  businessName: z.string().min(1, { message: "Business name is required" }),
-  businessDescription: z
-    .string()
-    .min(1, { message: "Business description is required" }),
-  businessEmail: z.string().email({ message: "Invalid email address" }),
-  businessPhone: z
-    .string()
-    .min(10, { message: "Phone number must be at least 10 digits" }),
-  addressLine1: z.string().min(1, { message: "Address line 1 is required" }),
-  addressLine2: z.string().optional(),
-  addressLine3: z.string().optional(),
-  postalCode: z
-    .string()
-    .min(5, { message: "Postal code must be at least 5 characters" }),
-  websiteUrl: z.string().url({ message: "Invalid website URL" }).optional(),
-  logoUrl: z.string().url({ message: "Invalid logo URL" }).optional(),
+	businessName: z.string().min(1, { message: "Business name is required" }),
+	businessDescription: z
+		.string()
+		.min(1, { message: "Business description is required" }),
+	businessEmail: z.string().email({ message: "Invalid email address" }),
+	businessPhone: z
+		.string()
+		.min(10, { message: "Phone number must be at least 10 digits" }),
+	addressLine1: z.string().min(1, { message: "Address line 1 is required" }),
+	addressLine2: z.string().optional(),
+	addressLine3: z.string().optional(),
+	postalCode: z
+		.string()
+		.min(5, { message: "Postal code must be at least 5 characters" }),
+	websiteUrl: z.string().url({ message: "Invalid website URL" }).optional(),
+	logoUrl: z.string().url({ message: "Invalid logo URL" }).optional(),
 });
